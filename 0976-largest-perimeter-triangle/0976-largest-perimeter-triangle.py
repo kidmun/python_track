d6398f5e31396a0a5dd05cd23128f8b5992b39b1
Kidus Guade <99493375+kidmun@users.noreply.github.com>
2022-12-24 00:15:51 -0800

    Time: 244 ms (76.26%), Space: 15.4 MB (48.78%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
@@ -3,12 +3,12 @@ class Solution:
         nums.sort()
         maximum = 0
         right = len(nums) - 1 
-        left = right - 2 
-        while left >= 0:
-            if (nums[left] + nums[left + 1]) > nums[right]:
-                maximum = max(maximum, nums[left] + nums[left + 1] + nums[right]) 
+         
+        while (right - 2) >= 0:
+            if (nums[right-2] + nums[right - 1]) > nums[right]:
+                maximum = max(maximum, nums[right - 2] + nums[right - 1] + nums[right]) 
             right -= 1
-            left = right - 2 
+            
         return maximum
             
                     
