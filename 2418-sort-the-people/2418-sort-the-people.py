87943ae3f7f58a317228c920bf203f8b5aa49d62
Kidus Guade <99493375+kidmun@users.noreply.github.com>
2023-01-17 08:10:46 -0800

    Time: 666 ms (9.54%), Space: 14.4 MB (42.89%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -14,14 +14,23 @@ class Solution:
             
 #         return names
         
-        for i in range(len(names) - 1):
-            large = i
-            for j in range(i + 1, len(names)):
-                if heights[j] > heights[large]:
-                    large = j
-            heights[large], heights[i] = heights[i], heights[large]
+        # for i in range(len(names) - 1):
+        #     large = i
+        #     for j in range(i + 1, len(names)):
+        #         if heights[j] > heights[large]:
+        #             large = j
+        #     heights[large], heights[i] = heights[i], heights[large]
+        
+        for i in range(len(heights) - 1):
+            j = i + 1
+            cur = heights[j]
+            while j > 0 and cur > heights[j-1]:
+                heights[j] = heights[j-1]
+                j -= 1
+            
+            heights[j] = cur
                 
-    
+      
         for i, h in enumerate(heights):
             names[i] = obj[h]
             
