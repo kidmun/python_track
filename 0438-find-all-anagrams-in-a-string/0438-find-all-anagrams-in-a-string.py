d8edea8d2048894082f6f9b5ac855620606d90bb
Kidus Guade <99493375+kidmun@users.noreply.github.com>
2023-02-22 15:20:15 +0300

    Time: 325 ms (34.67%), Space: 15.2 MB (26.13%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -3,18 +3,17 @@ class Solution:
         target = Counter(p)
         window = Counter(s[:len(p)])
         left = 1
-        right = len(p)
         ans = []
         if window == target:
             ans.append(0) 
         window[s[0]] -= 1
-        while right < len(s):
-            window[s[right]] += 1
+        for i in range(len(p), len(s)):
+            window[s[i]] += 1
             if window == target:
                 ans.append(left)
             window[s[left]] -= 1
             left += 1
-            right += 1
+           
         return ans
                 
             
