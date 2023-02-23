ffc4f232b2a7d20a6a3b72a4cfdb0ee86a2a5582
Kidus Guade <99493375+kidmun@users.noreply.github.com>
2023-02-23 15:46:22 +0300

    Time: 84 ms (30.08%), Space: 14.3 MB (80.90%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,22 +1,13 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        count = {}
-        
+        count = Counter()
         for trip in trips:
-        
-            if trip[1] in count:
-                count[trip[1]] += trip[0]
-            else:
-                count[trip[1]] = trip[0]
-            if trip[2] in count:
-                count[trip[2]] -= trip[0]
-            else:
-                count[trip[2]] = -trip[0]
+            count[trip[1]] += trip[0]
+            count[trip[2]] -= trip[0]
         cur = 0
        
         for p in sorted(count):
             cur += count[p]
-     
             if cur > capacity:
                 return False
         return True
