9d7dcfb1c0559f3464a5c3908230d7ff02fdef8b
Kidus Guade <99493375+kidmun@users.noreply.github.com>
2023-02-27 11:54:50 +0300

    Time: 38 ms (80.43%), Space: 13.9 MB (54.24%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -3,8 +3,9 @@ class Solution:
         ans = 0
         for i in range(k):
             ans += min(tickets[k], tickets[i])
-        for i in range(k + 1, len(tickets)):  
-            ans += min(tickets[k] - 1, tickets[i])
+        val = tickets[k] - 1
+        for i in range(k + 1, len(tickets)):
+            ans += min(val, tickets[i])
         return ans + tickets[k]
             
             
