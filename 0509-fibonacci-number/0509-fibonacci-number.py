7f06e5fdd52c2d596298724781de4dd94ed02faf
Kidus Guade <99493375+kidmun@users.noreply.github.com>
2023-02-28 10:51:16 +0300

    Time: 1518 ms (5.00%), Space: 13.7 MB (93.56%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,6 +1,10 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n < 2:
-            return n
-        return self.fib(n-1) + self.fib(n-2)
+        count = {0:0, 1:1}
+        if n in count:
+            return count[n]
+        val1 = self.fib(n-1)
+        if n not in count:
+            count[n] = val1
+        return val1 + self.fib(n-2)
     
