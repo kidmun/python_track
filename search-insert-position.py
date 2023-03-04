84d39d423085bae1cd0d5417312f99454a1b7b32
Kidus Guade <99493375+kidmun@users.noreply.github.com>
2023-03-04 23:11:56 +0300

    added search-insert-position.python

---
+++ search-insert-position.py
@@ -0,0 +1,19 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        left = 0
+        right = n - 1
+        while left <= right:
+            mid = (left + right)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        if nums[mid] > target:
+            return mid
+        else:
+            return mid + 1
+
+        return mid
