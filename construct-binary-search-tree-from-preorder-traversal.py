93f794451c62f216d4e19ac7406a622e3d45a82f
Kidus Guade <99493375+kidmun@users.noreply.github.com>
2023-03-24 12:20:37 -0700

    added construct-binary-search-tree-from-preorder-traversal.python

--- construct-binary-search-tree-from-preorder-traversal.py
+++ construct-binary-search-tree-from-preorder-traversal.py
@@ -6,29 +6,14 @@
 #         self.right = right
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
-        pref = [0]
-        pref.extend(list(accumulate(preorder)))
         def bst(left, right):     
             if left > right:
-                return None 
-        
+                return None    
             if left == right:
-                return TreeNode(preorder[left])
-            
+                return TreeNode(preorder[left])      
             ind = bisect_right(preorder[left+1:right+1], preorder[left])
             ind += (left + 1)
-            ans = TreeNode(preorder[left],  bst(left + 1,ind - 1),bst(ind,right))
-            
-            return ans
-        # preorder.sort()
-        # def bst(mid, left, right):
-        #     print(preorder[mid], left, right)
-        #     if left >= mid or right <= mid:
-        #         return TreeNode(preorder[mid])
-        #     mid1 = (left + mid) // 2 
-        #     mid2 = (mid + 1 + right) // 2
-        #     return TreeNode(preorder[mid], bst(mid1, left, mid - 1),bst(mid2, mid + 1,right))
+            return TreeNode(preorder[left],  bst(left + 1,ind - 1),bst(ind,right)) 
         n = len(preorder)
         ans = bst(0,n - 1)
-        print(ans)
         return ans
