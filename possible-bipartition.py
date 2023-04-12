0639ca9a4cfc9dc043e9323cdc7da3ee7bbb2e04
Kidus Guade <99493375+kidmun@users.noreply.github.com>
2023-04-12 07:46:41 -0700

    added possible-bipartition.python

---
+++ possible-bipartition.py
@@ -0,0 +1,24 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+
+        graph = defaultdict(list)
+        colors = [-1] * n
+        
+        for dis in dislikes:
+            graph[dis[0]].append(dis[1])
+            graph[dis[1]].append(dis[0])
+        print(graph)
+        def dfs(node, node_color):
+            colors[node - 1] = node_color
+            for ne in graph[node]:
+                if colors[ne - 1] == node_color:
+                    return False
+                if colors[ne - 1] == -1:
+                    if not dfs(ne, 1 - node_color):
+                        return False
+            return True
+        for i in range(1, n+1):
+            if colors[i - 1] == -1: 
+                if not dfs(i, 0):                
+                    return False
+        return True
