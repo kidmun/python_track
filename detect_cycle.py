d5e6ec259bb2f328809e73b9a873ab52784c0af2
Kidus Guade <99493375+kidmun@users.noreply.github.com>
2023-05-16 00:52:39 -0700

    Update detect_cycle.py

--- detect_cycle.py
+++ detect_cycle.py
@@ -2,28 +2,21 @@ from typing import List
 class Solution:
     #Function to detect cycle in an undirected graph.
     def isCycle(self, V: int, adj: List[List[int]]) -> bool:
-        indegree = [0] * v
-     
-        def dfs(cur):
+        indegree = [0] * V
+        def dfs(cur, par):
             if indegree[cur] == 2:
                 return False
             if indegree[cur] == 1:
                 return True
             indegree[cur] = 1
             for ne in adj[cur]:
-                if ne != cur:
-                    if dfs(ne):
+                if ne != par:
+                    if dfs(ne, cur):
                         return True
             indegree[cur] = 2
             return False
-        for i in range(v):
+        for i in range(V):
             if indegree[i] == 0:
-                if dfs(i):
+                if dfs(i, -1):
                     return True
         return False
-               
-            
-                
-                    
-        
-        #Code here
