892656bcd5541e07da89389f7d52995335c0a04f
Kidus Guade <99493375+kidmun@users.noreply.github.com>
2023-09-18 01:05:43 -0700

    added 2-keys-keyboard.python

--- 2-keys-keyboard.py
+++ 2-keys-keyboard.py
@@ -1,16 +1,20 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        if n == 1:
-            return 0
-        primes = []
-        i = 2
-        while i**2 <= n:
-            while n % i == 0 and n > 1:
-                primes.append(i)
-                n = n // i
-            i += 1
-        if n > 1:
-            primes.append(n)
-        if primes:
-            return sum(primes)
-        return n
+        # primes = []
+        # i = 2
+        # while i**2 <= n:
+        #     while n % i == 0 and n > 1:
+        #         primes.append(i)
+        #         n = n // i
+        #     i += 1
+        # if n > 1:
+        #     primes.append(n)
+        # return sum(primes)
+        dp = [0] * (n + 1)
+        dp[1] = 0
+        for i in range(2, n + 1):
+            x = i - 1
+            while i % x != 0:
+                x -= 1
+            dp[i] = dp[x] + i // x
+        return dp[n]
