f170a1dadbdda250f4a60f77924fa50979a04c03
Kidus Guade <99493375+kidmun@users.noreply.github.com>
2023-09-26 01:10:08 -0700

    added reducing-dishes.python

--- reducing-dishes.py
+++ reducing-dishes.py
@@ -2,20 +2,12 @@ class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
         n = len(satisfaction)
         satisfaction.sort()
-        if satisfaction[-1] <= 0:
-            return 0
-        total = sum(satisfaction)
-        ind = None
-        for i in range(n):
-            if total < 0:
-                total -= satisfaction[i]
-            else:
-                ind = i
-                break
-          
-        ans = 0
-        count = 1
-        for x in range(ind, n):
-            ans += count * satisfaction[x]
-            count += 1
-        return ans
+        memo = {}
+        def dp(ind, mul):
+            if ind == n - 1:
+                return max(0, mul * satisfaction[ind])
+            if (ind, mul) in memo:
+                return memo[(ind,mul)]
+            memo[(ind, mul)] = max(dp(ind + 1,mul + 1) + mul * satisfaction[ind] ,dp(ind + 1,mul))
+            return memo[(ind, mul)]
+        return dp(0, 1)
