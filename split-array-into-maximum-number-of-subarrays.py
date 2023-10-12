4016f3399d8da805dc9f52a3dd4c8c6116a27a3c
Kidus Guade <99493375+kidmun@users.noreply.github.com>
2023-10-12 06:37:02 -0700

    added split-array-into-maximum-number-of-subarrays.python

--- split-array-into-maximum-number-of-subarrays.py
+++ split-array-into-maximum-number-of-subarrays.py
@@ -1,25 +1,18 @@
 class Solution:
     def maxSubarrays(self, nums: List[int]) -> int:
         n = len(nums)
-        if n == 1:
-            return 1
         cur = nums[0]
         for i in range(1, n):
             cur &= nums[i]
-        x = None
+        x = 2 ** 31 - 1
         count = 0
         ind = 0
-       
         total = 0
         while ind < n:
-            if x == None:
-                x = nums[ind]
-            else:
-                x &= nums[ind]
+            x &= nums[ind]
             if  total + x <= cur:
                 count += 1
                 total += x
-                x = None
-           
+                x = 2 ** 31 - 1
             ind += 1
         return count
