57d11b837adff17ea0ea61ca0f9c9509ebf84cf7
Kidus Guade <99493375+kidmun@users.noreply.github.com>
2023-11-18 10:34:27 -0800

    Added solution for Sort Integers by The Power Value

--- leet-code-solutions/sort-integers-by-the-power-value.py
+++ leet-code-solutions/sort-integers-by-the-power-value.py
@@ -1,22 +1,25 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
+        memo = {}
         def power_of_two(num):
             return int(math.log(num, 2)) == ceil(math.log(num, 2))
-        def find_power(num, count):
-            # print(num, count, "shsh")
+        def find_power(num):
             if power_of_two(num):
-                return count + int(math.log(num, 2))
+                return int(math.log(num, 2))
+            if num in memo:
+                return memo[num]
             if num % 2 == 0:
-                return find_power(num // 2, count + 1)
-            return find_power(num * 3 + 1, count + 1)
+                val = find_power(num // 2)
+            else:
+                val = find_power(num * 3 + 1)
+            memo[num] = val + 1
+            return memo[num]
         ans = []
         for i in range(lo, hi + 1):
-            ans.append((find_power(i, 0), i))
+            ans.append((find_power(i), i))
         ans.sort()
-        # print(ans)
         return ans[k - 1][1]
 
-        # print(find_power(3, 0))
-        # return 4
+        
 
     
